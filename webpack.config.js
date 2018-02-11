aeeeb70d475b7b54f0bf6c42ee0c55b8ed657b87
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2018-02-10 21:18:31 -0500

    add HTML loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   context: path.resolve(__dirname, 'src'),
 
   entry: {
+    // removing 'src' directory from entry point, since 'context' is taking care of that
     app: './app.js'
   },
 
@@ -15,6 +17,7 @@ const config = {
 
   module: {
     rules: [
+      //babel-loader
       {
         test: /\.js$/,
         include: /src/,
@@ -25,12 +28,18 @@ const config = {
             presets: ['env']
           }
         }
-      }
+      },
+      //html-loader
+      { test: /\.html$/, use: ['html-loader'] }
     ]
   },
 
   plugins: [
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    //html-webpack-plugin instantiation
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
   ],
 
   devServer: {
