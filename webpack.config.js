ee20643595d55b4f80c4df312bbfb380e0fc612d
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2018-02-10 21:11:26 -0500

    Clean dist. First plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    // relative path declaration
+    // removing 'src' directory from entry point, since 'context' is taking care of that
     app: './app.js'
   },
   output: {
-    filename: 'bundle.js',
-    // Output path using nodeJs path module
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    path: path.resolve(__dirname, 'dist'),
+    filename: './assets/js/[name].bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist/assets/media"),
+    compress: true,
+    port: 12000,
+    stats: 'errors-only',
+    open: true
+  },
+  devtool: 'inline-source-map'
+}
 
 module.exports = config;
