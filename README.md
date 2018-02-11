92a1fa8fc73a3d5a83020b7fefced40437471b9f
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2018-02-10 21:31:05 -0500

    Adds rest of loaders, scss, file, etc.

--- README.md
+++ README.md
@@ -1,12 +1,52 @@
 # Basic project starter
-This repo is my attempt to move from gulpjs to webpack. In time, it will probably replace https://github.com/jrelgin/prototype-template-fe
+This repo is my attempt to move from gulpjs to webpack. In time, it will probably replace this [little fella](https://github.com/jrelgin/prototype-template-fe) in time.
 
 
-## Getting started
-`npm install`
+## Install dependencies
 
-`npm run build`
+```
+npm install
+```
 
-`npm run build:production`
 
-`npm run watch`
+## Develop locally with webpack-dev-server
+1. Run
+
+```
+npm run dev
+```
+
+2. In your browser, navigate to: [http://localhost:2000/](http://localhost:2000/)
+## For bundled output
+
+```
+npm run build
+```
+
+## For production-ready output
+
+```
+npm run build:prod
+```
+
+## Loaders and Plugins used in this boilerplate
+
+### Loaders
+* babel-loader
+* html-loader
+* sass-loader
+* css-loader
+* style-loader
+* file-loader
+
+### Plugins
+* clean-webpack-plugin
+* extract-text-webpack-plugin
+* html-webpack-plugin
+
+
+Tons of help from [this article](https://medium.com/@nirjhor123/webpack-3-quickstarter-configure-webpack-from-scratch-30a6c394038a)
+
+
+### Todos
+* Add handlerbars.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,87 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path'),
+      webpack = require('webpack'),
+      CleanWebpackPlugin = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({ filename: './assets/css/app.css' });
 
 const config = {
+
+  // absolute path for project root
   context: path.resolve(__dirname, 'src'),
 
   entry: {
-    // removing 'src' directory from entry point, since 'context' is taking care of that
+    // relative path declaration
     app: './app.js'
   },
 
   output: {
+    // absolute path declaration
     path: path.resolve(__dirname, 'dist'),
     filename: './assets/js/[name].bundle.js'
   },
 
   module: {
     rules: [
-      //babel-loader
+
+      // babel-loader with 'env' preset
+      { test: /\.js$/, include: /src/, exclude: /node_modules/, use: { loader: "babel-loader", options: { presets: ['env'] } } },
+      // html-loader
+      { test: /\.html$/, use: ['html-loader'] },
+      // sass-loader with sourceMap activated
       {
-        test: /\.js$/,
-        include: /src/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['env']
-          }
-        }
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
       },
-      //html-loader
-      { test: /\.html$/, use: ['html-loader'] }
+      // file-loader(for images)
+      { test: /\.(jpg|png|gif|svg)$/, use: [ { loader: 'file-loader', options: { name: '[name].[ext]', outputPath: './assets/media/' } } ] },
+      // file-loader(for fonts)
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+
     ]
   },
 
   plugins: [
+    // cleaning up only 'dist' folder
     new CleanWebpackPlugin(['dist']),
-    //html-webpack-plugin instantiation
     new HtmlWebpackPlugin({
       template: 'index.html'
-    })
+    }),
+    // extract-text-webpack-plugin instance
+    extractPlugin
   ],
 
   devServer: {
+    // static files served from here
     contentBase: path.resolve(__dirname, "./dist/assets/media"),
     compress: true,
-    port: 12000,
+    // open app in localhost:2000
+    port: 2000,
     stats: 'errors-only',
     open: true
   },
 
   devtool: 'inline-source-map'
-}
+
+};
 
 module.exports = config;
